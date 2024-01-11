03a2c46a24bacf0ab82e969dd841564c76fee126
Krish Shah <krishshah@ucla.edu>
2024-01-11 15:31:51 -0800

    added comments

--- fr.py
+++ fr.py
@@ -9,11 +9,15 @@ engine = pyttsx3.init()
 
 
 def main():
+
+# generate encodings for similarity and recognition
     encodings, names = generate_encodings('./people/')
     # with open('people_encodings.pkl', 'rb') as f:
     #     encodings, names = pickle.load(f)
     print('generated encodings')
     print(encodings)
+
+# start videocam
     # 0 for phone, 0 for macbook, 1 for external webcam
     video = cv2.VideoCapture(0)
     _, image_frame = video.read()
@@ -22,16 +26,20 @@ def main():
     while video.isOpened():
 
         _, image_frame = video.read()
+
+# get resolution of the frame
         frame_width = int(video.get(3))
         frame_height = int(video.get(4))
 
         print('video ratio: ',frame_height,'x',frame_width)
 
+# look for face
         if not has_face(image_frame):
             print('no face')
             cv2.imshow('Video', image_frame)
             continue
         
+# check for similarity between encoded faces if face is found
         image_frame, encoding, (top, right, bottom, left) = find_face(image_frame) 
         results = fr.face_distance(encodings, encoding)
         match_i, similarity = np.argmin(results), 1 - np.min(results)
@@ -43,13 +51,13 @@ def main():
         
         cv2.putText(image_frame, f'{names[match_i]} ({100 * similarity:.2f}%)', (left, top - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0), 2)
          
-
+# face tracking relative to middle of frame
         face_midpoint = -((right + left)/2 - (frame_width/2))
         
         print('face coords: ',top,left)
         print('x midpoint: ', face_midpoint)
 
-
+# dictate name of person found
         engine.setProperty('rate', 150) 
         engine.say(f'Hello {names[match_i]}!')
         engine.runAndWait()
@@ -63,6 +71,7 @@ def main():
 
 def generate_encodings(directory, verbose=False):
     encodings, names = [], []
+#encode all images with people in './people'
     for i,filename in enumerate(os.listdir(directory)):
         f = os.path.join(directory, filename)
         if os.path.isfile(f):
@@ -73,16 +82,21 @@ def generate_encodings(directory, verbose=False):
     return encodings, names
 
 def has_face(img):
+#check how many faces are in frame and return true if there is at least 1
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return len(fr.face_locations(img)) > 0
 
 def find_face(img, name=None):
     # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+#get all faces in the frame
     faces = fr.face_locations(img)
     if len(faces) == 0:
         return None, None
     biggest_i = 0
     biggest_face = 0
+
+# calculate area of all the faces, and find which one is the biggest (~closest to camera)
     for i,(top,right,bottom,left) in enumerate(faces):
         face = (bottom - top) * (right - left)
         if face > biggest_face:
